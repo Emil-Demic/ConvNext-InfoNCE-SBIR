5304180bd123ab8e3ba3b9e5c8708230c52c2aaa
Emil-Demic <77987406+Emil-Demic@users.noreply.github.com>
2024-10-25 15:27:54 +0200

    change default vals

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ parser.add_argument('--save', action='store_true', default=False,
 parser.add_argument('--val_unseen', action='store_true', default=False,
                     help='Use unseen user train/val split')
 
-parser.add_argument('--epochs', type=int, default=20,
+parser.add_argument('--epochs', type=int, default=10,
                     help='Number of epochs to train.')
 
 parser.add_argument('--lr', type=float, default=0.0001,
@@ -21,7 +21,7 @@ parser.add_argument('--lr', type=float, default=0.0001,
 parser.add_argument('--temp', type=float, default=0.05,
                     help='Temperature parameter for InfoNCE loss.')
 
-parser.add_argument('--batch_size', type=int, default=5,
+parser.add_argument('--batch_size', type=int, default=60,
                     help='Number of samples in each batch.')
 
 parser.add_argument("--model_path", type=str, default='',
