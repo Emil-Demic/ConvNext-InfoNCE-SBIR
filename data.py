d83d3da62b77ac2e828b83846a17bddf60ed6aad
Emil-Demic <77987406+Emil-Demic@users.noreply.github.com>
2024-11-25 19:52:28 +0100

    normal

--- data.py
+++ data.py
@@ -36,16 +36,16 @@ class DatasetFSCOCO(Dataset):
         return len(self.files)
 
     def __getitem__(self, idx):
-        sketch_path = os.path.join(self.root, "raw_data", self.files[idx][:-4] + ".json")
+        # sketch_path = os.path.join(self.root, "raw_data", self.files[idx][:-4] + ".json")
 
-        # sketch_path = os.path.join(self.root, "raster_sketches", self.files[idx])
+        sketch_path = os.path.join(self.root, "raster_sketches", self.files[idx])
         image_path = os.path.join(self.root, "images", self.files[idx])
 
-        sketch = json.load(open(sketch_path))
-        sketch = drawPNG(sketch)
-        sketch = Image.fromarray(sketch)
+        # sketch = json.load(open(sketch_path))
+        # sketch = drawPNG(sketch)
+        # sketch = Image.fromarray(sketch)
 
-        # sketch = Image.open(sketch_path)
+        sketch = Image.open(sketch_path)
         image = Image.open(image_path)
 
         if self.transforms_sketch:
